3836cb486dae8663c1784850e2053747e8030dc6
unknown <m.w.g.nelson@gmail.com>
2021-02-13 12:50:39 +0000

    Added Node dependencies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express= require('express')
 const app =express();
 const routes = require('./routes') // includes the routes.js file
 var path =require('path');
+
 app.use(express.json()) 
 app.use(routes) 
 app.use(express.static(path.join(__dirname, '')))
@@ -12,6 +13,8 @@ app.use(express.static(path.join(__dirname, 'Client')))
 
 //THE ABOVE LINE IS NEEDED TO CORRECTLY SERVE JS SCRIPT ASWELL AS
 // HTML STATIC FILES FROM CLIENT FOLDER!!
+
+
 app.listen(process.env.PORT,()=>{
   console.log("The Server is running on port:"+process.env.PORT)
 
